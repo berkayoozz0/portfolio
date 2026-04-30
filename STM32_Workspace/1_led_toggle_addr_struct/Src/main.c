b0507b253cc09a75001336bc899a901d1f6153ae
berkayoozz0 <gravityartz55@gmail.com>
2026-04-30 20:11:50 +0300

    Add files via upload

---
+++ STM32_Workspace/1_led_toggle_addr_struct/Src/main.c
@@ -0,0 +1,67 @@
+#include <stdint.h>
+
+#define PERIPH_BASE  	 		(0x40000000UL)
+#define AHB1PERIPH_OFFSET		(0x00020000UL)
+
+#define AHB1PERIPH_BASE 		(PERIPH_BASE + AHB1PERIPH_OFFSET) //0x4000 0000 + 0x0002 0000 = 0x4002 0000
+#define GPIOA_OFFSET            (0x0000UL)  // 0x 0000 0000
+#define GPIOA_BASE				(AHB1PERIPH_BASE + GPIOA_OFFSET)
+
+#define RCC_OFFSET				(0x3800UL)
+#define RCC_BASE				(AHB1PERIPH_BASE + RCC_OFFSET)
+
+#define GPIOAEN    				(1U<<0) //0b 0000 0000 0000 0000 0000 0000 0000 0001
+
+#define PIN5					(1U<<5)
+#define LED_PIN					 PIN5
+
+#define __IO volatile
+
+
+typedef struct
+{
+	  volatile uint32_t DUMMY[12];
+	  volatile uint32_t AHB1ENR;     /*!< RCC AHB1 peripheral clock register,    Address offset: 0x30 */
+}RCC_TypeDef;
+
+
+
+typedef struct
+{
+	  volatile uint32_t MODER;    /*!< GPIO port mode register,               Address offset: 0x00      */
+	  volatile uint32_t DUMMY[4];
+	  volatile uint32_t ODR;      /*!< GPIO port output data register,        Address offset: 0x14      */
+
+}GPIO_TypeDef;
+
+#define RCC    ((RCC_TypeDef*) RCC_BASE)
+#define GPIOA  ((GPIO_TypeDef*) GPIOA_BASE)
+
+
+int main(void)
+{
+
+	// 1. Enable clock access to GPIOA.
+	//RCC_AHB1EN_R |= GPIOAEN;
+	  RCC->AHB1ENR |= GPIOAEN;
+
+	// 2. Set PA5 as an output pin.
+	//GPIOA_MODE_R |=  (1U<<10);
+	//GPIOA_MODE_R &=~ (1U<<11);
+
+	  GPIOA->MODER |=  (1U<<10);
+	  GPIOA->MODER &=~ (1U<<11);
+
+	while(1)
+	{
+		// 3. Set PA5 high. (Ledi yakalım)
+		//GPIOA_OD_R |= LED_PIN;
+
+		//4. Experiment 2: Toggle PA5
+		//GPIOA_OD_R ^= LED_PIN;
+		  GPIOA->ODR ^= LED_PIN;
+
+		for(int i=0;i<1000000;i++){}  //delay için for döngüsü açtık
+
+	}
+}
