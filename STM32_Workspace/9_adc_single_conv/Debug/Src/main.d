4fc2a4bb8953151e388747211fc623b6ad4e1329
berkayoozz0 <gravityartz55@gmail.com>
2026-04-30 20:16:35 +0300

    Add files via upload

---
+++ STM32_Workspace/9_adc_single_conv/Debug/Src/main.d
@@ -0,0 +1,20 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/uart.h ../Inc/adc.h
+C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/unknown/Documents/STM32_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/uart.h:
+../Inc/adc.h:
